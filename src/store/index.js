5e49f3ac4aef4ae7183877c6846c2baab32d61ab
codyWSL <adwingsx@aim.com>
2022-09-20 13:10:23 -0400

    redux slice user, logout incomplete

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loggerMiddleware from "redux-logger";
+import userReducer from "./userSlice";
+// import productReducer from "./productSlice";
+// import cartReducer from './cartSlice'
+export default configureStore({
+	reducer: {
+		user: userReducer,
+		// product: productReducer,
+		// cart:cartReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(loggerMiddleware),
+});
