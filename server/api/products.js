b465cb2ce5c0f248ee2394e563be1050158bb975
codyWSL <adwingsx@aim.com>
2022-09-21 17:48:07 -0400

    api gatekeeping update

--- server/api/products.js
+++ server/api/products.js
@@ -1,73 +1,77 @@
 const router = require('express').Router();
 
-const { Product } = require('../db')
-
+const { Product } = require('../db');
+const { isAdmin, requireToken } = require('./gatekeepingMiddleware');
 
 // get All products
-router.get ( '/', (req, res, next) => {
-    return Product.findAll().then(product => {
-        res.json(product);
-        return null
-    }).catch(next);
+router.get('/', (req, res, next) => {
+  return Product.findAll()
+    .then((product) => {
+      res.json(product);
+      return null;
+    })
+    .catch(next);
 });
 
 // get product by price
 router.get('/price/:price', (req, res, next) => {
-    return Product.findAll({
-        where: {
-            price: {
-               $lt: req.params.price
-            }
-        }
-    })
-    .then(product => res.json(product))
+  return Product.findAll({
+    where: {
+      price: {
+        $lt: req.params.price,
+      },
+    },
+  })
+    .then((product) => res.json(product))
     .catch(next);
-})
+});
 
 // get product by tag
 router.get('/tag/:tag', (req, res, next) => {
-    return Product.findAll({
-        where: {
-            tags: {
-                $contains: [req.params.tag]
-            }
-        }
-    })
-    .then(product => res.json(product))
+  return Product.findAll({
+    where: {
+      tags: {
+        $contains: [req.params.tag],
+      },
+    },
+  })
+    .then((product) => res.json(product))
     .catch(next);
-})
+});
 
 //create new product
-router.post("/add-product",  (req, res, next) => {
-    return Product.create(req.body)
-      .then(product => res.status(201).send(product))
-      .catch(next);
-  });
+router.post('/add-product', requireToken, isAdmin, (req, res, next) => {
+  console.log('papi', req.user.isAdmin);
+  return Product.create(req.body)
+    .then((product) => res.status(201).send(product))
+    .catch(next);
+});
 
-// delete product 
-  router.delete("/:id",  (req, res, next) => {
-    return Product.findById(req.params.id)
-      .then(product => {
-        if (product) {
-          return Product.destroy({ where: { id: req.params.id } }).then(() => res.sendStatus(200))
-        } else res.sendStatus(404);
-      })
-      .catch(next);
-  });
-
-  // edit product
-  router.put("/:id",  (req, res, next) => {
-    return Product.update(req.body, {
-      where: {
-        id: req.params.id
-      },
-      returning: true
+// delete product
+router.delete('/:id', requireToken, isAdmin, (req, res, next) => {
+  return Product.findById(req.params.id)
+    .then((product) => {
+      if (product) {
+        return Product.destroy({ where: { id: req.params.id } }).then(() =>
+          res.sendStatus(200)
+        );
+      } else res.sendStatus(404);
     })
-      .then(result => {
-        result[1][0] ? res.json(result[1][0]) : res.sendStatus(404);
-      })
-      .catch(next);
-  });
-  
+    .catch(next);
+});
 
-module.exports = router
+// edit product
+router.put('/:id', requireToken, isAdmin, (req, res, next) => {
+  return Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+    returning: true,
+  })
+    .then((result) => {
+      result[1][0] ? res.json(result[1][0]) : res.sendStatus(404);
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,78 +1,87 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 const userSlice = createSlice({
-	name: "user",
-	initialState: {},
-	reducers: {
-		login: (state, action) => {
-			state = action.payload;
-			return state;
-		},
-		logout: (state) => {
-			state = {};
-			return state;
-		},
-		update: (state, action) => {
-			state = action.payload;
-			return state;
-		},
-	},
+  name: 'user',
+  initialState: {},
+  reducers: {
+    login: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+    logout: (state) => {
+      state = {};
+      return state;
+    },
+    update: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+  },
 });
 export default userSlice.reducer;
 export const { login, logout, update } = userSlice.actions;
 
 export const attemptTokenLogin = () => async (dispatch) => {
-	const token = window.localStorage.getItem("token");
-	if (token) {
-		const { data: userInfo } = await axios.post(
-			"/api/auth",
-			{},
-			{
-				headers: {
-					authorization: token,
-				},
-			}
-		);
-		dispatch(login(userInfo));
-	}
+  const token = window.localStorage.getItem('token');
+  if (token) {
+    const { data: userInfo } = await axios.post(
+      '/api/auth',
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    dispatch(login(userInfo));
+  }
 };
 export const attemptPsswordLogin = (loginInfo) => async (dispatch) => {
-	try {
-		const { data: token } = await axios.post("/api/auth/login", loginInfo);
-		window.localStorage.setItem("token", token);
-		dispatch(attemptTokenLogin());
-	} catch (error) {
-		return error;
-	}
+  try {
+    const { data: token } = await axios.post('/api/auth/login', loginInfo);
+    window.localStorage.setItem('token', token);
+    dispatch(attemptTokenLogin());
+  } catch (error) {
+    return error;
+  }
 };
 export const logoutUser = () => {
-	return (dispatch) => {
-		dispatch(logout());
-		window.localStorage.removeItem("token");
-	};
+  return (dispatch) => {
+    dispatch(logout());
+    window.localStorage.removeItem('token');
+  };
 };
 export const createUser = (userDetails) => {
-	return async (dispatch) => {
-		try {
-			const { data: user } = await axios.post("/api/users", {
-				...userDetails,
-			});
-			if (user) {
-				attemptPsswordLogin({
-					username: userDetails.username,
-					password: userDetails.password,
-				})(dispatch);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
-}
-export const updateUser = (userDetails, userId) =>  async (dispatch) => {
-        try {
-            const { data: user} = await axios.put(`/api/users/${userId}`, userDetails)
-            dispatch(update(user));
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      const { data: user } = await axios.post('/api/users', {
+        ...userDetails,
+      });
+      if (user) {
+        attemptPsswordLogin({
+          username: userDetails.username,
+          password: userDetails.password,
+        })(dispatch);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+};
+export const updateUser = (userDetails, userId) => async (dispatch) => {
+  try {
+    const token = window.localStorage.getItem('token');
+    const { data: user } = await axios.put(
+      `/api/users/${userId}`,
+      userDetails,
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    dispatch(update(user));
+  } catch (error) {
+    console.log(error);
+  }
+};
