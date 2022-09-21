457debb5a94ff84a88d771cdc50bf0027f867e9f
codyWSL <adwingsx@aim.com>
2022-09-21 18:01:14 -0400

    fatch all users auth

--- src/store/allUsersSlice.js
+++ src/store/allUsersSlice.js
@@ -1,32 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 const allUsersSlice = createSlice({
-    name: "allUsers",
-    initialState: [],
-    reducers: {
-        _setAllUsers: (state, action) => {
-            state = action.payload;
-            return state;
-        },
-        _unsetAllUsers: (state) => {
-            state = [];
-            return state;
-        }
-    }
-})
+  name: 'allUsers',
+  initialState: [],
+  reducers: {
+    _setAllUsers: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+    _unsetAllUsers: (state) => {
+      state = [];
+      return state;
+    },
+  },
+});
 
 export default allUsersSlice.reducer;
 export const { _setAllUsers, _unsetAllUsers } = allUsersSlice.actions;
 
 export const fetchAllUsers = (user) => async (dispatch) => {
-    const { data: users } = await axios.get('/api/users', {
-        headers: {
-            admin: user.isAdmin
-        }
-    });
-    console.log('Logging users in slice: ', users)
-    dispatch(_setAllUsers(users))
-}
+  const token = window.localStorage.getItem('token');
+  const { data: users } = await axios.get('/api/users', {
+    headers: {
+      authorization: token,
+    },
+  });
+  console.log('Logging users in slice: ', users);
+  dispatch(_setAllUsers(users));
+};
 export const unsetAllUsers = () => (dispatch) => {
-    dispatch(_unsetAllUsers())
-}
+  dispatch(_unsetAllUsers());
+};
