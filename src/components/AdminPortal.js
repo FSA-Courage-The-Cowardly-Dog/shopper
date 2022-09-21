66929bc1ece87ccc5082e58c4e51283e4fbbd939
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-09-21 08:54:30 -0500

    adding comments for myself to organize thoughts/workflow

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -27,3 +27,6 @@ export default AdminPortal;
 // will work on viewing all user information first; will want to make an express route to serve up all users, but check that user is an admin
 // maybe look into passing in the user object calling the route, or some other parameters; could even just pass in the user.isAdmin value
 // will likely need to create new slice for the store, maybe allUsersSlice or something
+
+// in express route, check an if statement to make sure user authorized; if not, res.send("unauthorized access");
+// that way, I'm thinking that maybe will protect the api route? unsure.
