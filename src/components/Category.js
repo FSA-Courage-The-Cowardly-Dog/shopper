9015b56adb3dd23a4155f5927bdcfcea6dfaecf7
Robby0501 <robbyulrich@hotmail.com>
2022-09-23 17:44:21 -0400

    category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { attemptGetTagList } from '../store/productSlice';
 import { useParams } from 'react-router-dom';
+import "../styling/Category.css"
+import { Link } from 'react-router-dom';
 
 function Category() {
   const dispatch = useDispatch();
@@ -13,12 +15,19 @@ function Category() {
   }, []);
 
   const products = useSelector((state) => state.product.productList);
-
+  
   return (
+
     <div>
+      <section className='displayCostumes'>
       {products.map((product) => (
-        <img src={product.img} />
+        <Link to={`/singleproduct/${product.id}`} className='costume'>
+          <img className="image" src={product.img}/>
+          <p className='costumeName'>{product.name}</p>
+          <span className='costumePrice'>${product.price/100}</span>
+        </Link>
       ))}
+      </section>
     </div>
   );
 }
