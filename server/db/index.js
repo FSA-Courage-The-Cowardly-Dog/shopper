b8c82693a329aea2a4aafd84b2870bdc384766c9
codyWSL <adwingsx@aim.com>
2022-09-23 14:44:25 -0400

    seedmini

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const User = require('./User');
 const Order = require('./Order');
 const LineItem = require('./LineItem');
 const { users } = require('./seed/users.json');
+const { users2 } = require('./seed/users2.json');
 const { products } = require('./seed/products.json');
 const { tags } = require('./seed/tags.json');
 const { products2 } = require('./seed/products2.json');
@@ -38,6 +39,7 @@ const syncAndSeed = async () => {
     });
     await Promise.all(tags.map((tags) => Tag.create(tags)));
     await Promise.all(users.map((user) => User.create(user)));
+    await Promise.all(users2.map((user) => User.create(user)));
     await Promise.all(
       products.map(async (product) => {
         const newprod = await Product.create(product);
@@ -65,7 +67,43 @@ const syncAndSeed = async () => {
     console.error(err);
   }
 };
-
+const syncAndSeedmini = async () => {
+  try {
+    await conn.sync({ force: true });
+    await User.create({
+      username: 'test',
+      email: 'test@apple.com',
+      password: 'password',
+      firstName: 'test first name',
+      lastName: 'test last name',
+      isAdmin: false,
+    });
+    await User.create({
+      username: 'admin',
+      email: 'admin@apple.com',
+      password: 'password',
+      firstName: 'admin first name',
+      lastName: 'admin last name',
+      isAdmin: true,
+    });
+    await Promise.all(tags.map((tags) => Tag.create(tags)));
+    await Promise.all(users.map((user) => User.create(user)));
+    await Promise.all(
+      products.map(async (product) => {
+        const newprod = await Product.create(product);
+        if (product.tags) {
+          product.tags.forEach(async (tag) => {
+            let newtag = await Tag.findOne({ where: { name: tag.name } });
+            await newprod.addTag(newtag);
+          });
+        }
+      })
+    );
+    console.log(`Seeding successful!`);
+  } catch (err) {
+    console.error(err);
+  }
+};
 module.exports = {
   conn,
   User,
@@ -74,4 +112,5 @@ module.exports = {
   Tag,
   LineItem,
   syncAndSeed,
+  syncAndSeedmini,
 };
