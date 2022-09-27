5348f930a1a34ff278c3b61275945d1d5df75efd
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-09-27 17:20:57 -0500

    almost have stripe payment test hooked up; am able to redirect to stripe checkout page; need to just hook up running user cart checkout method after purchase confirmation

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,5 +1,7 @@
 const { User } = require('../db');
 const router = require('express').Router();
+// need to change this out later; set key as env variable
+const stripe = require('stripe')('sk_test_51LmjNEJX4F0kIK6BftbaAP3GhwySc2hKsY2WuIAzc6KEHET7P18zyzMGrjBcNyeXIDQrQ8h9APxgUf876j6zMa5r00PGA4w0L3')
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -56,6 +58,25 @@ router.put('/usercart/checkout', async (req, res, next) => {
     const order = await user.createOrder()
     await order.update({checkoutPrice: req.body.total})
     res.send(order)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// need to write an axios.post req to below url
+// need to include line_items in below format
+
+router.post('/usercart/create-checkout-session', async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: JSON.parse(req.body.lineItems),
+
+      success_url: `${process.env.CLIENT_URL}/cart/orderconfirmation`,
+      cancel_url: `${process.env.CLIENT_URL}/cart/checkout`
+    })
+    res.send({url: session.url})
   } catch (err) {
     next(err)
   }
--- src/index.js
+++ src/index.js
@@ -6,24 +6,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-// for stripe integration
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-// stripe publishable test key
-const stripePromise = loadStripe('pk_test_51LmjNEJX4F0kIK6BNSw021mmdUkQXeznZtMIwTQUnsSYRjLaMNWZ2HOaQvCCNAlDfClPj13TImasMXxXKuPIB6M900aqrXvak4')
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const options = {
-  // passing the client secret obtained from the server
-  // unsure how to hook this up yet; will look into later
-  clientSecret: '{{CLIENT_SECRET}}',
-};
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <Elements stripe={stripePromise}>
-        <App />
-      </Elements>
+      <App />
     </Provider>
   </BrowserRouter>
 );
