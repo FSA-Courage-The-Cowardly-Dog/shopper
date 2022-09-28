34cde9765f4adb80db732349df6ff817e0eafaa7
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-09-28 08:09:12 -0500

    adjuster Header component and css to help with changing window size; still need to look at it more for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,23 +15,24 @@ function Header() {
           <Link  to="/" className='temp-link'>
                <img className='headerLogo' src ='https://downtownjacksonville.org/wp-content/uploads/2020/10/jackolanding-06.png'/>
          </Link>
-         <div className="searchbar">
-              <input className={user.id ? 'signedInSearch' :'searchBar'} placeholder='Search' />
-           </div>
-          
-            {user.id ? <></> :
+         <div className='searchbar-div'>
+            <div className="searchbar">
+               <input className={user.id ? 'signedInSearch' :'searchBar'} placeholder='Search' />
+            </div>
+            <SearchIcon className='search' fontSize='large' />
+            </div>
+         </div>
+
+      <div className={user.id ? "headerRight" : 'headerright'}>
+         <Link to="/cart" className='temp'>
+            <ShoppingCartIcon className='carticon' fontSize='large' />
+         </Link>
+         {user.id ? <></> :
                <Link className='signIn' to='/signin'>
                   < AccountCircleIcon className='accounticon' fontSize='large'/>
                   <p className='loginLink'>Sign In</p>
                </Link>
             }
-        </div>
-
-      <div className={user.id ? "headerRight" : 'headerright'}>
-      <SearchIcon className='search' fontSize='large' />
-         <Link to="/cart" className='temp'>
-            <ShoppingCartIcon className='carticon' fontSize='large' />
-         </Link>
          </div>
       </div>
 
