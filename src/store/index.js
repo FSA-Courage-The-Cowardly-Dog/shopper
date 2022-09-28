9b5cad99d5205b157010b09e724f24ba31545be9
codyWSL <adwingsx@aim.com>
2022-09-28 10:50:12 -0400

    disable logger in production

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@ import loggerMiddleware from 'redux-logger';
 import userReducer from './userSlice';
 import allUsersReducer from './allUsersSlice';
 import productReducer from './productSlice';
-import cartReducer from './cartSlice'
-import orderHistoryReducer from './orderHistorySlice'
+import cartReducer from './cartSlice';
+import orderHistoryReducer from './orderHistorySlice';
 export default configureStore({
   reducer: {
     user: userReducer,
@@ -13,6 +13,11 @@ export default configureStore({
     cart: cartReducer,
     orderHistory: orderHistoryReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(loggerMiddleware),
+  middleware: (getDefaultMiddleware) => {
+    if (process.env.NODE_ENV === 'production') {
+      getDefaultMiddleware();
+    } else {
+      getDefaultMiddleware().concat(loggerMiddleware);
+    }
+  },
 });
